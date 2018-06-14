213d8c5f268aaa9dfc266a3c6c2e12e549d95a7b
RunyardDeveloper <39923930+Runyard@users.noreply.github.com>
2018-06-14 21:19:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,7 +11,7 @@ client.login(process.env.TOKEN)
 ///Avertissement en marche
 client.on("ready", () => {
   console.log("SKY: Je suis prêt")
-  client.user.setGame(`.help | SkyBOT <3);
+  client.user.setGame(`.help | SkyBOT <3`);
 });
   
 ///La version du BOT
