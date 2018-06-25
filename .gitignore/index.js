3e24668fe2b709d95d29daa851b03441dca2ed00
XXX82183289D <39923930+9283HDKUE@users.noreply.github.com>
2018-06-25 14:57:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -371,11 +371,6 @@ if(!message.content.startsWith(prefix)) return;
       .setColor("#FFFFFF")
       message.channel.sendMessage(skydon_embed)
     }
-  
-  client.on("guildMemberAdd", member => {
-let welcomechannel = member.guild.channels.find("name", "🌐bienvenue");
-if (!welcomechannel) return
-welcomechannel.send(`Bienvenue ${member}`);
     
     ///Commande .add
     if(message.content === prefix + "add"){
