afa176ce2e078ce807f1a8bcb78ba2e2b670a489
XXX82183289D <39923930+9283HDKUE@users.noreply.github.com>
2018-07-14 16:26:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,7 +10,7 @@ client.login(process.env.TOKEN)
 
 ////Avertissement en marche
 client.on("ready", () => {
-  client.user.setActivity(`.help | .invite | SkyTeam`);
+  client.user.setActivity(`.help | .invite | SkyTeam®`);
  });
    
  ///////////////////////////////////////////////////////////DEBUT DE LA COMMANDE .VERSION//////////////////////////////////////////////////////////
