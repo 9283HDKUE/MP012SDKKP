47ae8e830d904b9a050a03f16aec85090a581c77
XXX82183289D <39923930+9283HDKUE@users.noreply.github.com>
2018-07-18 15:44:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -267,7 +267,7 @@ message.author.send(helpmusic_embed);
 
         if(!args[0]) return message.channel.send("**ERREUR** Merci de préciser un nombre de message à supprimer.")
         message.channel.bulkDelete(args[0]).then(() => {
-            message.channel.send(`***:white_check_mark: OK | ${args[0]} messages supprimés du salon !***`).then(msg => msg.delete(5000))
+            message.channel.send(`:white_check_mark: ***OK | ${args[0]} messages supprimés du salon !***`).then(msg => msg.delete(5000))
         });
 }
  ///////////////////////////////////////////////////////////FIN DE LA COMMANDE .CLEAR//////////////////////////////////////////////////////////
